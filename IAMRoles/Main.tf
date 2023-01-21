f3528841b4a69af7e29c97c533a70af4c88e23d3
JdurgaPrasad <70344680+JdurgaPrasad@users.noreply.github.com>
2023-01-22 01:15:04 +0530

    Update Main.tf

--- IAMRoles/Main.tf
+++ IAMRoles/Main.tf
@@ -1,20 +1,20 @@
 terraform {
-	required_providers {
-		aws = {
-			source = "hashicorp/aws"
-		}
-	}
-
-	backend "remote" {
-		hostname = "app.terraform.io"
-		organization = "CloudQuickLabs"
-
-		workspaces {
-			name = "AWSBackup"
-		}
-	}
+  backend "s3" {
+    bucket         = "terraform-tfstate-backend-test-2023"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "us-west-2"
+    ##dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
 }
 
 provider "aws" {
-	region = "us-east-1"
+	region = "us-west-2"
 }
